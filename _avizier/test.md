e03d754e6e01be73d224d999f8a6c24adcab75c2
prssic <andreiardei299@gmail.com>
2026-09-17 22:19:41 +0300

    gg

---
+++ _avizier/test.md
@@ -0,0 +1,5 @@
+---
+title: Test
+layout: avizier-post
+---
+Hello
---
+++ fix.js
@@ -0,0 +1,34 @@
+﻿const fs = require('fs');
+
+const map = {
+    'Č›': 'ț',
+    'Ă®': 'î',
+    'Äƒ': 'ă',
+    'Č™': 'ș',
+    'Ă˘': 'â',
+    'ĂŽ': 'Î',
+    'Čš': 'Ț',
+    'Č˜': 'Ș',
+    'Ä‚': 'Ă',
+    'Â ': ' ',
+    'Ă ': 'à',
+    'ĂĄ': 'á',
+    'Ă¨': 'è',
+    'ĂŠ': 'é',
+    'Ă­': 'í',
+    'ĂŻ': 'ï',
+    'Ăł': 'ó',
+    'Ăş': 'ú',
+    'Ă§': 'ç'
+};
+
+const files = ['index.html', 'board.html', 'studenti.html', 'evenimente.html', 'facultate.html'];
+
+files.forEach(f => {
+    let content = fs.readFileSync(f, 'utf8');
+    for (const [bad, good] of Object.entries(map)) {
+        content = content.split(bad).join(good);
+    }
+    fs.writeFileSync(f, content);
+});
+console.log('Done');
